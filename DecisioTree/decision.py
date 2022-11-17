77b9c8d03111a406cba57c6ea65ced32fc94ef88
SamOctGonzAzpeitia <A01704696@itesm.mx>
2022-11-17 16:17:28 -0600

    add pdf

--- DecisioTree/decision.py
+++ DecisioTree/decision.py
@@ -8,6 +8,7 @@ import graphviz
 from sklearn import preprocessing
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 from sklearn.tree import plot_tree
 from sklearn.metrics import confusion_matrix
 
@@ -17,7 +18,7 @@ df=pd.get_dummies(data=df, drop_first=True)
 #remove nan
 df=df.dropna()
 #split data}
-print(df.head())
+
 X = df.drop('satisfaction_satisfied', axis=1)
 y = df['satisfaction_satisfied']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -29,11 +30,25 @@ print(clf.score(X_test, y_test))
 plt.figure(figsize=(20,20))
 plot_tree(clf, filled=True, rounded=True, class_names=['Unsatisfied', 'Satisfied'], feature_names=X.columns, fontsize=8)
 plt.show()
+
+
 #random forest
-rf = RandomForestClassifier(n_estimators=100, max_depth=5)
+rf = RandomForestClassifier(n_estimators=200, max_depth=5)
 rf.fit(X_train, y_train)
 y_pred = rf.predict(X_test)
 print(confusion_matrix(y_test, y_pred))
 print(rf.score(X_test, y_test))
 
+
+ 
+# creation of correlation matrix
+corrM = df.corr()
+print(corrM)
+
+#data visualization
+sb.heatmap(corrM, annot=True)
+plt.show()
+
+#clasification report
+print(classification_report(y_test, y_pred))
 
